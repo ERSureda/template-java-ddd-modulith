ec1a28cff878fd44aa7bebb1ab68eaaedb36a56f
ERSureda <eribas.sureda@gmail.com>
2026-09-06 22:32:21 +0200

    D1

--- src/main/java/com/template/api/ApiApplication.java
+++ src/main/java/com/template/api/ApiApplication.java
@@ -1,4 +1,4 @@
-package com.taxai.api;
+package com.template.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/template/api/ApiApplicationTests.java
+++ src/test/java/com/template/api/ApiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.taxai.api;
+package com.template.api;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
