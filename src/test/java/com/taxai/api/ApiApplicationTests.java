3225011c0ba52929a54123bbcc5617859a7a878a
ERSureda <eribas.sureda@gmail.com>
2026-09-06 22:08:40 +0200

    D1

---
+++ src/test/java/com/taxai/api/ApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.taxai.api;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@DisplayName("Application Context Tests")
+class ApiApplicationTests {
+
+	@Test
+	@DisplayName("Should successfully load the Spring application context")
+	void contextLoads() {
+	}
+
+}
